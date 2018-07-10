63fc244511671ee74c4e926e9e9abc758032d3fe
Michael Vaquier <michaelvaquier@Michaels-MacBook-Pro.local>
2018-07-09 21:50:28 -0400

    Profile page started

---
+++ whoshomemobile/iOS/ProfileViewController.cs
@@ -0,0 +1,63 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace whoshomemobile.iOS
+{
+    public partial class ProfileViewController : UIViewController
+    {
+        private const string _confirmString = "Confirm";
+        private const string _editString = "Edit";
+        
+        private bool _editingUsername
+        {
+            get
+            {
+                return UsernameTextBox.Enabled;
+            }
+        }
+
+        public ProfileViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            UsernameActionButton.TouchUpInside += UsernameActionClick;
+            UsernameCancelButton.TouchUpInside += UsernameCancelClick;
+        }
+
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void UsernameActionClick(object sender, EventArgs e)
+        {
+            if (!_editingUsername)
+            {
+                UsernameTextBox.Enabled = true;
+                UsernameCancelButton.Hidden = false;
+                UsernameActionButton.SetTitle(_confirmString, UIControlState.Normal);
+            }
+            else
+            {
+                UsernameTextBox.Enabled = false;
+                UsernameCancelButton.Hidden = true;
+                UsernameActionButton.SetTitle(_editString, UIControlState.Normal);
+            }
+        }
+
+        private void UsernameCancelClick(object sender, EventArgs e)
+        {
+            UsernameTextBox.Enabled = false;
+            UsernameCancelButton.Hidden = true;
+            UsernameActionButton.SetTitle(_editString, UIControlState.Normal);
+        }
+    }
+}
---
+++ whoshomemobile/iOS/ProfileViewController.designer.cs
@@ -0,0 +1,164 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace whoshomemobile.iOS
+{
+    [Register ("ProfileViewController")]
+    partial class ProfileViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton FullNameActionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton FullNameCancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField FullNameTextBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel InformationLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton MacAddressActionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton MacAddressCancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField MacAddressTextBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PasswordActionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PasswordCancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PasswordTextBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PiIDActionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PiIDCancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PiIDTextBox { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton UsernameActionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton UsernameCancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField UsernameTextBox { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (FullNameActionButton != null) {
+                FullNameActionButton.Dispose ();
+                FullNameActionButton = null;
+            }
+
+            if (FullNameCancelButton != null) {
+                FullNameCancelButton.Dispose ();
+                FullNameCancelButton = null;
+            }
+
+            if (FullNameTextBox != null) {
+                FullNameTextBox.Dispose ();
+                FullNameTextBox = null;
+            }
+
+            if (InformationLabel != null) {
+                InformationLabel.Dispose ();
+                InformationLabel = null;
+            }
+
+            if (MacAddressActionButton != null) {
+                MacAddressActionButton.Dispose ();
+                MacAddressActionButton = null;
+            }
+
+            if (MacAddressCancelButton != null) {
+                MacAddressCancelButton.Dispose ();
+                MacAddressCancelButton = null;
+            }
+
+            if (MacAddressTextBox != null) {
+                MacAddressTextBox.Dispose ();
+                MacAddressTextBox = null;
+            }
+
+            if (PasswordActionButton != null) {
+                PasswordActionButton.Dispose ();
+                PasswordActionButton = null;
+            }
+
+            if (PasswordCancelButton != null) {
+                PasswordCancelButton.Dispose ();
+                PasswordCancelButton = null;
+            }
+
+            if (PasswordTextBox != null) {
+                PasswordTextBox.Dispose ();
+                PasswordTextBox = null;
+            }
+
+            if (PiIDActionButton != null) {
+                PiIDActionButton.Dispose ();
+                PiIDActionButton = null;
+            }
+
+            if (PiIDCancelButton != null) {
+                PiIDCancelButton.Dispose ();
+                PiIDCancelButton = null;
+            }
+
+            if (PiIDTextBox != null) {
+                PiIDTextBox.Dispose ();
+                PiIDTextBox = null;
+            }
+
+            if (UsernameActionButton != null) {
+                UsernameActionButton.Dispose ();
+                UsernameActionButton = null;
+            }
+
+            if (UsernameCancelButton != null) {
+                UsernameCancelButton.Dispose ();
+                UsernameCancelButton = null;
+            }
+
+            if (UsernameTextBox != null) {
+                UsernameTextBox.Dispose ();
+                UsernameTextBox = null;
+            }
+        }
+    }
+}
