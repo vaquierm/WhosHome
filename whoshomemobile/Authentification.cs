f106660643274f9f8992457bd1d53f647108d524
Michael Vaquier <michaelvaquier74@gmail.com>
2018-07-30 20:53:01 -0400

    Updated name of connection string for IoT Hub

--- whoshomemobile/Authentification.cs
+++ whoshomemobile/Authentification.cs
@@ -8,7 +8,7 @@ namespace whoshomemobile
         public const string PublicUserCollectionName = "Public user data collection name here";
         public const string PrivateUsersCollectionName = "Private user data collection name here";
         
-        public const string IoTServiceConnectionString = "Service connection string here";
+        public const string IoTOwnerConnectionString = "IoT Hub owner connection string here";
         public const string DBEndpointUri = "Database endpoint URI here";
         public const string DBKey = "Database key here";
     }
